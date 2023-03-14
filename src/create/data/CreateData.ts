28007c81357c65993c61b6cf48758ad0ed90386e
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-03-14 22:52:58 +0200

    v1.0.1

--- src/create/data/CreateData.ts
+++ src/create/data/CreateData.ts
@@ -1,18 +1,35 @@
 import {object, z} from 'zod';
 import * as Path from "path";
 
-interface ICreateData<T extends Array<any>, K extends z.Schema, M extends ReadonlyArray<(...args: any) => any>> {
+interface ICreateData<T extends Array<any>, K extends z.Schema, M extends ReadonlyArray<(...args: any) => any>, C extends ((...args: T) => Promise<any>) | ((...args: T) => any)> {
     Schema: K,
-    Source: (...args: T) => Promise<any>;
+    Source: C;
     middleware: M;
 }
 
 type LastElementType<T extends readonly any[], K> = T extends readonly [...infer _, infer L] ? L : K;
 
 
-function CreateData<T extends Array<any>, K extends z.Schema, M extends ReadonlyArray<(...args: any) => any>>({Schema, Source, middleware} : ICreateData<T, K, M>) {
-    return async (...args: T): Promise<ReturnType<LastElementType<typeof middleware, () => z.infer<typeof Schema>>>> => {
-        const result = await Source(...args);
+
+type Return<T, D> = T extends (...args: any[]) => Promise<any> ? Promise<D> : D;
+
+
+function CreateData<T extends Array<any>, K extends z.Schema, M extends ReadonlyArray<(...args: any) => any>, C extends ((...args: T) => Promise<any>) | ((...args: T) => any)>({Schema, Source, middleware} : ICreateData<T, K, M, C>) {
+    type ReturnData = ReturnType<LastElementType<typeof middleware, () => z.infer<typeof Schema>>>;
+
+    return (...args: T): Return<typeof Source, ReturnData> => {
+        if (Source.constructor.name === 'AsyncFunction') {
+            Source(...args)
+                .then(result => {
+                    Schema.parse(result);
+                    let finalRes = result as z.infer<typeof Schema>;
+                    for (const md of middleware) {
+                        finalRes = md(finalRes);
+                    }
+                    return finalRes;
+                })
+        }
+        const result = Source(...args);
         Schema.parse(result);
         let finalRes = result as z.infer<typeof Schema>;
         for (const md of middleware) {
@@ -21,7 +38,6 @@ function CreateData<T extends Array<any>, K extends z.Schema, M extends Readonly
         return finalRes;
     }
 };
-
 
 interface IDataProvider<T extends Array<any>> {
     (...props: T): any
