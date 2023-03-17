1bd08fd8033da0c6c47cc336a92cb0b4aa6be067
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-03-17 23:07:22 +0200

    v1.0.2

--- src/create/component/CreateComponent.tsx
+++ src/create/component/CreateComponent.tsx
@@ -2,19 +2,21 @@ import React, { FC, ReactElement, ReactNode, useMemo } from "react";
 import { IService } from "../service/CreateService";
 import { IProvider } from "../provider/CreateProvider";
 
-export interface ICreateComponentConfig<T> {
-    readonly providers: FC<{children: any}>[];
+type TProvider<K> = FC<{children: any, props?: K}> | { provider: FC<{children: any, props?: K}>, props?: K }
+
+export interface ICreateComponentConfig<T, K> {
+    readonly providers: Array<TProvider<K>>;
     View: FC<T>;
 }
 
-function CreateComponent<T>({View, providers = []}: ICreateComponentConfig<T>) {
+function CreateComponent<T, K>({View, providers = []}: ICreateComponentConfig<T, K>) {
     const Component: FC<T> = (props) => {
         const render = View(props);
         return providers.reduceRight((acc, el) => {
-            const Provider = el;
+            const [Provider, props] = typeof el === 'function' ? [el, undefined] : [el.provider, el.props];
             if (!Provider) throw new Error(`hook ${el} has no Provider`);
             return (
-                <Provider>
+                <Provider props={props}>
                     {acc}
                 </Provider>
             )
