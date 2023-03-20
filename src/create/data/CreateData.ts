270b7de40c82bae4f4f31f371051dc0c3291100d
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-03-20 21:50:13 +0200

    v1.0.9

--- src/create/data/CreateData.ts
+++ src/create/data/CreateData.ts
@@ -9,13 +9,17 @@ interface ICreateData<T extends Array<any>, K extends z.Schema, M extends Readon
 
 type LastElementType<T extends readonly any[], K> = T extends readonly [...infer _, infer L] ? L : K;
 
+type UnionToIntersection<U> =
+    (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never
+
+type MiddlewareChanges<T extends readonly any[]> = T extends readonly ((...args: any) => infer L)[] ? L : never;
 
 
 type Return<T, D> = T extends (...args: any[]) => Promise<any> ? Promise<D> : D;
 
 
 function CreateData<T extends Array<any>, K extends z.Schema, M extends ReadonlyArray<(...args: any) => any>, C extends ((...args: T) => Promise<any>) | ((...args: T) => any)>({Schema, Source, middleware} : ICreateData<T, K, M, C>) {
-    type ReturnData = ReturnType<LastElementType<typeof middleware, () => z.infer<typeof Schema>>>;
+    type ReturnData = z.infer<typeof Schema> & UnionToIntersection<MiddlewareChanges<typeof middleware>>;
 
     return (...args: T): Return<typeof Source, ReturnData> => {
         if (Source.constructor.name === 'AsyncFunction') {
