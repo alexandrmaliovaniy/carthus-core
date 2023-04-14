58ff4e2869b839f1d9bb0439e3813ed3c88452bb
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-04-14 23:24:17 +0300

    v1.1.9

--- src/create/component/CreateComponent.tsx
+++ src/create/component/CreateComponent.tsx
@@ -1,46 +1,81 @@
-import React, { FC, ReactElement, ReactNode, useMemo } from "react";
+import React, {FC, FunctionComponent, ReactElement, ReactNode, useMemo} from "react";
 import { IService } from "../service/CreateService";
 import { IProvider } from "../provider/CreateProvider";
 
-type TProviderComponent<K> = FC<{children?: ReactNode} & K>;
-type TProviderWithProps<K> = { provider: TProviderComponent<K>, props?: K };
-type TProvider<K> = TProviderComponent<K> | TProviderWithProps<K>;
+type TProviderComponent = FC<{children?: ReactNode}>;
 
-export interface ICreateComponentConfig<T, K> {
-    readonly providers: Array<TProvider<K>>;
+type Return<T, D> = T extends (...args: any[]) => Promise<any> ? Promise<D> : D;
+
+type OptionalPropertyNames<T> =
+    { [K in keyof T]-?: ({} extends { [P in K]: T[K] } ? K : never) }[keyof T];
+
+type SpreadProperties<L, R, K extends keyof L & keyof R> =
+    { [P in K]: L[P] | Exclude<R[P], undefined> };
+
+
+type CommonArrayKeys<L, R, K = keyof L & keyof R, P = keyof K> = L[K] extends Array<any> ? R[K] extends Array<any> ? K : never : never;
+
+type MergeArrays<L, R, K extends keyof L & keyof R> =
+    { [P in K]: L[P] extends Array<infer M> ? R[P] extends Array<infer G> ? SpreadTwo<M, G>[] : R[P] : R[P] }
+
+type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
+
+type SpreadTwo<L, R> = Id<
+    & Pick<L, Exclude<keyof L, keyof R>>
+    & Pick<R, Exclude<Exclude<keyof R, OptionalPropertyNames<R>>, CommonArrayKeys<L, R>>>
+    & Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>>
+    & MergeArrays<L, R, keyof L & keyof R>
+    & SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+    >;
+
+
+type Spread<A extends ReadonlyArray<FunctionComponent<any> | unknown>> = A extends [infer L, ...infer R]
+    ? L extends FunctionComponent<infer C>
+        ? SpreadTwo<C, Spread<R>>
+        : 123
+    : null;
+
+
+type ArrayToIntersection<T extends readonly any[]> = T extends readonly [React.FC<infer Head>, ...infer Tail]
+    ? [Head, ...MergeArray<Tail>]
+    : never;
+
+type MergeArray<T extends readonly any[]> = ArrayToIntersection<T>
+
+export interface ICreateComponentConfig<T, K extends ReadonlyArray<any>> {
+    readonly providers: K;
     View: FC<T>;
 }
 
-function renderNestedComponents<K>(components: TProviderWithProps<K>[]) {
+function renderNestedComponents<K, T>(components: TProviderComponent[], actualProps: T) {
     // Base case: if there are no components left to render, return null
     if (components.length === 0) {
         return null;
     }
 
-    const {provider : Component, props} = components[0];
+    const Component = components[0];
 
     if (components.length === 1) {
-        return <Component {...props} />
+        return <Component {...actualProps} />
     }
 
     // Get the first component in the array
 
 
     // Recursively render the rest of the components in the array
-    const nestedComponents = renderNestedComponents(components.slice(1));
+    const nestedComponents = renderNestedComponents(components.slice(1), actualProps);
 
     // Render the current component and its nested components
-    return <Component {...props}>{nestedComponents}</Component>;
+    return <Component {...actualProps}>{nestedComponents}</Component>;
 }
 
-function CreateComponent<T, K>({View, providers = []}: ICreateComponentConfig<T, K>) {
-    const Component: FC<T> = (props) => {
-        const providersWithProps = providers.map(provider => typeof provider === 'function' ? ({provider: provider, props: {}}) as TProviderWithProps<K> : provider);
-        providersWithProps.push({
+function CreateComponent<T, K extends readonly any[]>({View, providers }: ICreateComponentConfig<T, K>) {
+    const Component: FC<Spread<[FC<T>, ...K]>> = (props) => {
+        providers.push({
             provider: View,
             props: props
         })
-        return renderNestedComponents<K>(providersWithProps);
+        return renderNestedComponents<K, T>(providers, props);
     }
     return Component;
 }
